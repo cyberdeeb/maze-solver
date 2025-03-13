079c5df063db3fc1c4e14b793ad5764cfc0c4327
cyberdeeb <abrahamdeeb@gmail.com>
2025-03-13 13:52:57 -0700

    Set up Point and Line class, draw function

--- graphics.py
+++ graphics.py
@@ -14,6 +14,9 @@ class Window:
         self.__root.update_idletasks()
         self.__root.update()
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
     def wait_for_close(self):
         self.__running = True
         while self.__running:
@@ -22,3 +25,16 @@ class Window:
 
     def close(self):
         self.__running = False
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
