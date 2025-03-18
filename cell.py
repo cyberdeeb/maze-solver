dbb7c94387168411dc770edbd87dea21a6647d2d
cyberdeeb <abrahamdeeb@gmail.com>
2025-03-18 11:04:37 -0700

    Added Maze class and tests

--- cell.py
+++ cell.py
@@ -1,7 +1,7 @@
 from graphics import Line, Point
 
 class Cell:
-    def __init__(self, window):
+    def __init__(self, window=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
@@ -30,3 +30,21 @@ class Cell:
         if self.has_bottom_wall == True:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._window.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        half_length = abs(self._x2 - self._x1) // 2
+        x_center = half_length + self._x1
+        y_center = half_length + self._y1
+
+        half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
+        x_center2 = half_length2 + to_cell._x1
+        y_center2 = half_length2 + to_cell._y1
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self._window.draw_line(line, fill_color)
+
+        
